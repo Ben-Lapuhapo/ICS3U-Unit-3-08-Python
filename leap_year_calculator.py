f514862065532daa8548bef7acc745bfb0d800c2
Ben Lapuhapo <ben.lapuhapo@mths.ca>
2019-10-20 16:57:14 +0000

    Added leap year calculator

---
+++ leap_year_calculator.py
@@ -0,0 +1,35 @@
+# usr/bin/env python3
+
+# Created by Ben Lapuhapo
+# Created on Oct 2019
+# This Program determines if a year is a leap year
+
+
+def main():
+    while True:
+        try:
+            # calculates if year is a leap year
+            year = int(input("Enter a year: "))
+            print()
+        
+            if year % 4 == 0 and year % 100 != 0:
+                print("It is a leap year")
+            if year % 4 != 0:
+                print("It is not a leap year")
+            if year % 4 == 0 and year % 100 == 0:
+                if year % 400 != 0:
+                    print("It is not a leap year")
+                if year % 400 == 0 and year % 100 == 0:
+                    print("It is a leap year")
+
+        except ValueError:
+            print()
+            print("Invalid Input, Please Try Again")
+            print()
+
+        else:
+            break
+
+
+if __name__ == "__main__":
+    main()
